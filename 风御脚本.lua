01f6b83bd08c2e37b106e7e2970ef6896c6b339f
FengYu-3 <1926190957@qq.com>
2025-07-06 04:51:55 +0800

    Add files via upload

--- 风御脚本.lua
+++ 风御脚本.lua
@@ -6,43 +6,30 @@ local window = library:new("汽水脚本")--V1
 local creds = window:Tab("关于", "6031097229")
     local bin = creds:section("信息", true)
     
-    bin:Label("作者神青")    
-    bin:Label("神青脚本V6")
-    bin:Label("作者师傅：叶")
-    bin:Label("永久免费")
-    bin:Label("帮助者：鹤")
-    bin:Label("做完这个我绝定不更新去学艺了，下次不知道什么时候更新")
+    bin:Label("作者：齐静春")    
+    
+    bin:Button("QQ号", function()
+    setclipboard("1599267691")
+end)
+    
+
+    bin:Label("副作者：风御 X")
+    
+    bin:Button("QQ号", function()
+    setclipboard("风御 X：不处谢谢")
+end)
+    
+
     bin:Label("禁止倒卖")
     bin:Label("感谢支持")
-    bin:Label("欢迎你的使用")
-    bin:Label("神青大王nb")
-    bin:Label("原来的神青脚本已经发展了")
     
-    bin:Button("复制神青脚本主群", function()
-    setclipboard("1003591865")
-end)
-    bin:Button("复制神青脚本副群", function()
-    setclipboard("1015840930")
+    bin:Button("脚本主群", function()
+    setclipboard("1009641699")
 end)
     
 
     local credits = creds:section("Ul设置", true)
 
-credits:Toggle("移除UI辉光", "", false, function(state)
-        if state then
-            game:GetService("CoreGui")["frosty is cute"].Main.DropShadowHolder.Visible = false
-        else
-            game:GetService("CoreGui")["frosty is cute"].Main.DropShadowHolder.Visible = true
-        end
-    end)
-
-    credits:Toggle("彩虹UI", "", false, function(state)
-        if state then
-            game:GetService("CoreGui")["frosty is cute"].Main.Style = "DropShadow"
-        else
-            game:GetService("CoreGui")["frosty is cute"].Main.Style = "Custom"
-        end
-    end)
 credits:Toggle("脚本框架变小一点", "", false, function(state)
         if state then
         game:GetService("CoreGui")["frosty"].Main.Style = "DropShadow"
