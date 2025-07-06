f27dc8342235fe65a12dd697da1628f836d18370
FengYu-3 <1926190957@qq.com>
2025-07-06 17:35:01 +0800

    Add files via upload

--- 风御脚本.lua
+++ 风御脚本.lua
@@ -138,7 +138,7 @@ GunHook = hookmetamethod(game, "__namecall", function(self, ...)
         end;
         return GunHook(self, unpack(args));
 end);
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/wumingjiaoben/UI2.0/refs/heads/main/%E8%90%BD%E9%99%A8%E4%B8%AD%E5%BF%83ui%20(1).txt", true))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FengYu-3/FengY4/refs/heads/main/%E6%B1%BD%E6%B0%B4ui.lua", true))()
 ----------------------------------------------------------------------------------------------------------------------------------------
 local window = library:new("汽脚本")--V1
 ----------------------------------------------------------------------------------------------------------------------------------------
@@ -182,7 +182,7 @@ credits:Toggle("脚本框架变小一点", "", false, function(state)
             game:GetService("CoreGui")["frosty"]:Destroy()
     end)
 
-local creds = window:Tab("通用",'7733765398')
+local creds = window:Tab("通用",'6031097229')
 
 local credits = creds:section("通用脚本",true)
     credits:Textbox("快速跑步(推荐调2)", "tpwalking", "输入", function(king)
@@ -204,58 +204,7 @@ while tpwalking and hb:Wait() and chr and hum and hum.Parent do
 end
 end)
     
-local creds = window:Tab("哦",'7733765398')
+credits:Toggle("穿山甲", "NoClip", false, function(NC)
+  local Workspace = game:GetService("Workspace") local Players = game:GetService("Players") if NC then Clipon = true else Clipon = false end Stepped = game:GetService("RunService").Stepped:Connect(function() if not Clipon == false then for a, b in pairs(Workspace:GetChildren()) do if b.Name == Players.LocalPlayer.Name then for i, v in pairs(Workspace[Players.LocalPlayer.Name]:GetChildren()) do if v:IsA("BasePart") then v.CanCollide = false end end end end else Stepped:Disconnect() end end)
+    end)
 
-local credits = creds:section("哦，是你啊",true)
-
-local Players = about:Dropdown("选择玩家", 'Dropdown', dropdown, function(v)
-    playernamedied = v
-end)
-
-game.Players.ChildAdded:Connect(function(player)
-    dropdown[player.UserId] = player.Name
-    Players:AddOption(player.Name)
-end)
-
-game.Players.ChildRemoved:Connect(function(player)
-    Players:RemoveOption(player.Name)
-    for k, v in pairs(dropdown) do
-        if v == player.Name then
-            dropdown[k] = nil
-        end
-    end
-end)
-
-    credits:Button("传送到玩家旁边", function()
-    local HumRoot = game.Players.LocalPlayer.Character.HumanoidRootPart
-    local tp_player = game.Players:FindFirstChild(playernamedied)
-    if tp_player and tp_player.Character and tp_player.Character.HumanoidRootPart then
-        HumRoot.CFrame = tp_player.Character.HumanoidRootPart.CFrame + Vector3.new(0, 3, 0)
-        Notify("哦", "已经传送到玩家身边", "rbxassetid://", 5)
-    else
-        Notify("哦", "无法传送 玩家已消失", "rbxassetid://", 5)
-    end
-end)
-
-    credits:Button("把玩家传送过来", function()
-    local HumRoot = game.Players.LocalPlayer.Character.HumanoidRootPart
-    local tp_player = game.Players:FindFirstChild(playernamedied)
-    if tp_player and tp_player.Character and tp_player.Character.HumanoidRootPart then
-        tp_player.Character.HumanoidRootPart.CFrame = HumRoot.CFrame + Vector3.new(0, 3, 0)
-        Notify("哦", "已传送过来", "rbxassetid://", 5)
-    else
-        Notify("哦", "无法传送 玩家已消失", "rbxassetid://", 5)
-    end
-end)
-
-    about:Toggle("查看玩家", 'Toggleflag', false, function(state)
-    if state then
-        game:GetService('Workspace').CurrentCamera.CameraSubject =
-            game:GetService('Players'):FindFirstChild(playernamedied).Character.Humanoid
-            Notify("哦", "已开启", "rbxassetid://", 5)
-    else
-        Notify("哦", "已关闭", "rbxassetid://", 5)
-        local lp = game.Players.LocalPlayer
-        game:GetService('Workspace').CurrentCamera.CameraSubject = lp.Character.Humanoid
-    end
-end)
