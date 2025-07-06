1d049add650a3c03775c21c2993921d60b610cb2
FengYu-3 <1926190957@qq.com>
2025-07-07 02:22:46 +0800

    Add files via upload

--- 风御脚本.lua
+++ 风御脚本.lua
@@ -350,7 +350,6 @@ creditshubb:Textbox("自定义范围（尽量400以下）", "HitBox", "输入", 
     end)
 end)
     
-
 creditshubb:Button("范围10", function()
     _G.HeadSize = 10 _G.Disabled = true game:GetService('RunService').RenderStepped:connect(function() if _G.Disabled then for i,v in next, game:GetService('Players'):GetPlayers() do if v.Name ~= game:GetService('Players').LocalPlayer.Name then pcall(function() v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize) v.Character.HumanoidRootPart.Transparency = 0.7 v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue") v.Character.HumanoidRootPart.Material = "Neon" v.Character.HumanoidRootPart.CanCollide = false end) end end end end)
 end)
@@ -406,7 +405,7 @@ end)
 creditshubb:Button("范围300", function()
     _G.HeadSize = 300 _G.Disabled = true game:GetService('RunService').RenderStepped:connect(function() if _G.Disabled then for i,v in next, game:GetService('Players'):GetPlayers() do if v.Name ~= game:GetService('Players').LocalPlayer.Name then pcall(function() v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize) v.Character.HumanoidRootPart.Transparency = 0.7 v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue") v.Character.HumanoidRootPart.Material = "Neon" v.Character.HumanoidRootPart.CanCollide = false end) end end end end)
 end)
-    
+
 local creds = window:Tab("音乐",'6031097229')
 
 local credits = creds:section("音乐播放",true)
